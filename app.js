25b477e4829a5c399783a4b2e8ad6f29b2fb8d1d
magalipujol <magalicpujol@gmail.com>
2021-10-22 15:47:36 -0300

    ahora si bendito fooooooooooor

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let app = Vue.createApp({
 })
 
 app.component('activity-card', {
-    props: ['activities', "index"],
+    props: ['activity'],
     methods: {
 
     },
@@ -20,57 +20,12 @@ app.component('activity-card', {
     <div class="card" style="width: 18rem;">
     <img class="card-img-top activity-img" src="https://static.bigbox.com.ar/uploads/activity/7d70d0e1-50a7-4395-ad5f-ed3a7a723f2f.jpg" alt="Card image cap">
     <div class="card-body activity-card-body">
-      <h5 class="card-title activity-title">{{ this.activities[index]?.title }}</h5>
+      <h5 class="card-title activity-title">{{ this.activity?.title }}</h5>
       <p class="card-text activity-location">Palermo, CABA</p>
       <p class="card-text activity-description">Caipiroska a granel con vodka y picoteo de tu bar preferido, directo a la peurta de tu casa</p>
       <p class="card-text activity-points">5500 puntos</p>
     </div>
   </div>
-    `
-})
-
-app.component('cards-grid', {
-    props: ['activities'],
-    template: `
-    <div class="container">
-            <div class="row">
-                <div class="col-sm">
-                    <activity-card :activities="activities" :index="0"></activity-card>
-                </div>
-                <div class="col-sm">
-                    <activity-card :activities="activities" :index="1"></activity-card>
-                </div>
-                <div class="col-sm">
-                    <activity-card :activities="activities"></activity-card>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm">
-                    <activity-card :activities="activities"></activity-card>
-                </div>
-                <div class="col-sm">
-                    <activity-card :activities="activities"></activity-card>
-                </div>
-                <div class="col-sm">
-                    <activity-card :activities="activities"></activity-card>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm">
-                    <activity-card :activities="activities"></activity-card>
-                </div>
-                <div class="col-sm">
-                    <activity-card :activities="activities"></activity-card>
-                </div>
-                <div class="col-sm">
-                    <activity-card :activities="activities"></activity-card>
-                </div>
-            </div>
-        </div>
     `
 })
 
