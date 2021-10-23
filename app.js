2e95f823335467547caf32719d57699bb56d9714
magalipujol <magalicpujol@gmail.com>
2021-10-23 16:37:01 -0300

    agregué info dinámica a las cartas de actividad

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ let app = Vue.createApp({
         const response = await fetch('https://json-biglifeapp.herokuapp.com/activity?_page=1&_limit=9')
         data = await response.json()
         this.activities = await data
+        for (let activity of this.activities) {
+            activity.activity = JSON.parse(activity.activity)
+        }
+        console.log(this.activities)
     }
 })
 
@@ -18,12 +22,13 @@ app.component('activity-card', {
     },
     template: `
     <div class="card" style="width: 18rem;">
-    <img class="card-img-top activity-img" src="https://static.bigbox.com.ar/uploads/activity/7d70d0e1-50a7-4395-ad5f-ed3a7a723f2f.jpg" alt="Card image cap">
+    <img class="card-img-top activity-img" :src="this.activity?.activity.image[0]" alt="Card image cap">
     <div class="card-body activity-card-body">
       <h5 class="card-title activity-title">{{ this.activity?.title }}</h5>
-      <p class="card-text activity-location">Palermo, CABA</p>
-      <p class="card-text activity-description">Caipiroska a granel con vodka y picoteo de tu bar preferido, directo a la peurta de tu casa</p>
-      <p class="card-text activity-points">5500 puntos</p>
+      <p class="card-text activity-location">{{ this.activity?.activity.locations[0].province }}</p>
+      <p class="card-text activity-participants">{{ this.activity?.activity.participants }}</p>
+      <p class="card-text activity-description">{{ this.activity?.activity.description }}</p>
+      <p class="card-text activity-points">{{ this.activity?.points }} puntos</p>
     </div>
   </div>
     `
